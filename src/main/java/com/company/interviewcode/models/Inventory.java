5b37fa884498f1c0a34567cbaa13dd7f43e319e3
Mohit Sharma <chiragmohitsharma@gmail.com>
2026-05-21 23:40:57 +0530

    Same Models only

--- src/main/java/com/company/interviewcode/models/Inventory.java
+++ src/main/java/com/company/interviewcode/models/Inventory.java
@@ -8,6 +8,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Inventory {
+    // This is inventory only with multiple objects of products.
+    // Not written in this form -
+    // inventory class -> List<Products> as a field in it.
+    // It is same !!
     private Long Id;
     private String productName;
     private int quantity;
